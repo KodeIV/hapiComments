6e34c8b26ab926da4485e84adf33f1457de659b3
Natalie Newton <natali.newton523@gmail.com>
2014-10-29 16:23:33 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+var Hapi = require("hapi");
+//var joi = require("joi");
+//var bell = require("bell");
+var hapiAuthCookie = require("hapi-auth-cookie");
+var routes2 = require("./routes/routes2.js");
+
+var pack = new Hapi.Pack();
+var server = pack.server(+process.env.PORT, '0.0.0.0', {
+        debug: {
+            request: ['error']
+        }
+    });
+
+var dbOpts = {
+    "url": "mongodb://KodeIV:KoDeIv@linus.mongohq.com:10038/KodeIVMongo",
+    "settings": {
+        "db": {
+            "native_parser": false
+        }
+    }
+};
+
+
+server.pack.register(
+    {
+        plugin: require('hapi-mongodb'),
+        options: dbOpts
+    },
+
+    function (err) {
+    if (err) {
+            console.error(err);
+            throw err;
+        }
+    }
+);
+
+// server.pack.register(require('bell'), function (err) {
+
+//     server.auth.strategy('facebook', 'bell', {
+//         provider: 'facebook',
+//         password: 'hapiauth',
+//         clientId: '562715117207744',
+//         clientSecret: 'f33a41096a843319154ccb80ece9104e',
+//         isSecure: false     // Terrible idea but required if not using HTTPS
+//     });
+// });
+
+// server.pack.register(require('hapi-auth-cookie'), function (err) {  
+//     if (err) {
+//         throw err;
+//     }
+//   });
+
+//     // Set our strategy
+//     server.auth.strategy('session', 'cookie', {
+//         password: 'hapiauth', // cookie secret
+//         cookie: 'session', // Cookie name
+//         redirectTo: false, // Let's handle our own redirections
+//         isSecure: false, // required for non-https applications
+//         ttl: 24* 60 * 60 * 1000 // Set session to 1 day
+//     });
+
+if (!module.parent){
+	pack.start(function() {
+    	console.log("Hapi server started @", server.info.uri);
+
+	});
+}
+
+server.route(routes2);
---
+++ routes/routes2.js
@@ -0,0 +1,16 @@
+var handlers = require("../handlers/commentHandlers.js"); 
+
+module.exports = [
+	{
+		path: "/",
+		method: "GET",
+		handler: 
+			handlers.insertComment
+		
+	},
+
+	{
+		
+	}
+
+]
